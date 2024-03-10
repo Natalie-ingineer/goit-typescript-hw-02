a0478d2df73b0b4acea672a6df5576156b633243
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-10 11:41:03 +0200

    update

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,8 +3,8 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, K>(objA: T, objB: K): T & K {
-  return Object.assign({ objA, objB });
+function merge<T extends object, K>(objA: T, objB: K): T & K {
+  return Object.assign(objA, objB);
 }
 
 export {};
